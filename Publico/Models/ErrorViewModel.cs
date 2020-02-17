9d18c3fbaa19c91ea01addc5e29e64cea4ae8b4c
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-02-18 01:45:53 +0300

    [#30] fix send to back

--- Publico/Models/ErrorViewModel.cs
+++ Publico/Models/ErrorViewModel.cs
@@ -2,10 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace Publico.Models {
+    // Класс с методами для обработки ошибок
     public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
         public static IActionResult Error() {
             throw new Exception("Проблемы с моделью");
         }
--- Publico/wwwroot/js/AuthClient.js
+++ Publico/wwwroot/js/AuthClient.js
@@ -3,11 +3,12 @@ var app = new Vue({
 	el: '#app',
 	methods: {
 		// Регистрирует пользователя
-		onRegister: function () {	
+		onRegister: function () {
 			// Получение данных с форм ввода
 			var login = document.getElementById("exampleInputLogin").value;
 			var email = document.getElementById("exampleInputEmail").value;
 			var password = document.getElementById("exampleInputPassword").value;
+			var url = "https://localhost:44323/api/odata/auth/create";
 			// Объект с данными для бэка
 			var User = {
 				Login: login,
@@ -15,50 +16,33 @@ var app = new Vue({
 				Password: password
 			};
 			// Отправляет данные на бэк
-			$.ajax({
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				type: 'POST',
-				url: 'https://localhost:44323/api/odata/auth/create', 
-				data: JSON.stringify(User), 
-				dataType: 'json', 
-				success: function (data) {
-					console.log(data);
-				}, 
-				error: function (jqXHR, text, error) {
-					console.log('Ошибка выполнения');
-				}
-			});
+			axios.post(url, User)
+				.then((response) => {
+					console.log(response);
+				})
+				.catch((XMLHttpRequest, textStatus, errorThrown) => {
+					console.log("request send error");
+				});
 		},
 		// Проверяет существование пользователя в БД
 		onSignIn: function () {
 			// Получение данных с форм ввода
 			var login = document.getElementById("exampleInputLogin").value;
 			var password = document.getElementById("exampleInputPassword").value;
+			var url = "https://localhost:44323/api/odata/auth/signin";
 			// Объект с данными для бэка
 			var UserReg = {
 				Login: login,
 				Password: password
 			};
 			// Отправляет данные на бэк
-			$.ajax({
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				type: 'POST',
-				url: 'https://localhost:44323/api/odata/auth/signin',
-				data: JSON.stringify(UserReg),
-				dataType: 'json',
-				success: function (data) {
-					console.log(data);
-				},
-				error: function (jqXHR, text, error) {
-					console.log('Ошибка выполнения');
-				}
-			});
+			axios.post(url, UserReg)
+				.then((response) => {
+					console.log(response);
+				})
+				.catch((XMLHttpRequest, textStatus, errorThrown) => {
+					console.log("request send error");
+				});
 		}
 	}
 });
